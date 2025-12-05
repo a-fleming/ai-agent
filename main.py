50447f8a899e1021224ae2490047b77e63046a36
Aaron Fleming <amfleming15@gmail.com>
2025-12-04 20:11:24 -0800

    Add argparse to take prompt from provided argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -8,10 +10,14 @@ if not api_key:
     raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+
     client = genai.Client(api_key=api_key)
     model = "gemini-2.5-flash"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model=model, contents=contents)
+    response = client.models.generate_content(model=model, contents=messages)
     if not response.usage_metadata:
         raise RuntimeError("No usage metadata found in the response.")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
